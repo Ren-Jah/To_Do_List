d3ceadcb170f83c486d2059918b48480ea146f76
RenJah <khazix1313@gmail.com>
2022-12-14 21:18:04 +0300

    revert to 13.12 8pm

--- todolist/settings.py
+++ todolist/settings.py
@@ -74,8 +74,8 @@ DATABASES = {
         "NAME": env('POSTGRES_DB'),
         "USER": env('POSTGRES_USER'),
         "PASSWORD": env('POSTGRES_PASSWORD'),
-        "HOST": env('POSTGRES_HOST'),
-        "PORT": env('POSTGRES_PORT'),
+        "HOST": env('POSTGRES_HOST', default='127.0.0.1'),
+        "PORT": env('POSTGRES_PORT', default=5432),
     }
 }
 
