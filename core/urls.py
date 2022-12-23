dbd9a78b3c4f667a2cef0f13a96e3ad2737f164a
RenJah <khazix1313@gmail.com>
2022-12-23 13:57:41 +0300

    ouath upd

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from core import views
 
 urlpatterns = [
-    path('signup', views.RegistrationView.as_view()),
-    path('login', views.LoginView.as_view()),
-    path('profile', views.ProfileView.as_view()),
-    path('update_password', views.UpdatePasswordView.as_view()),
+    path("signup", views.RegistrationView.as_view()),
+    path("login", views.LoginView.as_view()),
+    path("profile", views.ProfileView.as_view()),
+    path("update_password", views.UpdatePasswordView.as_view()),
 ]
