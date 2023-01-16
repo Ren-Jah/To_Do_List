feec9d8edc44f860404e14bf06742ddfea4e8b38
RenJah <khazix1313@gmail.com>
2023-01-16 20:53:14 +0300

    init

--- todolist/urls.py
+++ todolist/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     path('core/', include('core.urls')),
     path('api/core/', include('core.urls')),
     path('goals/', include('goals.urls')),
-    path('bot/', include('bot.urls')),
     path('api/goals/', include('goals.urls')),
+    path('bot/', include('bot.urls')),
+    path('api/bot/', include('bot.urls')),
     path('oauth/', include("social_django.urls", namespace="social")),
 ]
